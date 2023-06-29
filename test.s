587112c16454614026a74c81833574321a21f89c
maxsofar <maxshermant@gmail.com>
2023-06-29 09:15:31 +0300

    Add files via upload

---
+++ test.s
@@ -0,0 +1,20 @@
+.text
+main:   # Put your code here
+        # add		t6, x0, x0
+        # beq		t6, x0, finish
+
+        lw      t0, 8(x0)
+        addi    t1, t0, 12
+        sw      t1, 16(x0)
+        beq     x0, x0, finish
+
+
+deadend: beq	t6, x0, deadend        
+
+finish:
+        lw		t4, 0(x0)
+        lw		t5, 4(x0)
+        sw		t5, 0xFF(t4)
+        beq		t6, x0, deadend
+
+
